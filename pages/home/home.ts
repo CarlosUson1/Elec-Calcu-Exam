170d41eb97b8b60478536aa720a7332c10fcc675
CarlosUson1 <carlos.uson090002@my.lsgh.edu.ph>
2021-04-20 13:08:12 +0800

    2 files modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -3,9 +3,23 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
 })
 export class HomePage {
+  powcon: number;
+  hour: number;
+  cost: number;
+  elec: number;
+  cpd: number;
+
+  calculateelec() {
+    this.elec = this.powcon * (this.hour/ 1000);
+    this.elec = parseFloat(this.elec.toFixed(2));
+
+    this.cpd = this.elec * this.cost;
+    this.cpd = parseFloat(this.cpd.toFixed(4));
+  }
 
   constructor(public navCtrl: NavController) {
 
